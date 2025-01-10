b5bd25e193eb1e219f2edb0cf8f0f39cbe44841a
Ayush Khatri <khatriayush2003@gmail.com>
2025-01-10 18:15:22 +0530

    Add: holding controller added

---
+++ server/controllers/holdings.js
@@ -0,0 +1,92 @@
+import prisma from "../db/db.js"
+
+const getAllHoldings = async (req, res) => {
+  try {
+    const allHoldings = await prisma.stock.findMany({});
+    res.status(200).json({ message: "Successfully fetched!", data: allHoldings });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+}
+
+const getHoldingById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const holding = await prisma.stock.findUnique({
+      where: {
+        id
+      }
+    });
+    res.status(200).json({ message: "Successfully fetched!", data: holding });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+};
+
+const addHolding = async (req, res) => {
+  try {
+    const { name, ticker, price, quantity } = req.body;
+    const newHolding = await prisma.stock.create({
+      data: {
+        name: name,
+        symbol: ticker,
+        purchasePrice: price,
+        quantity: quantity
+      }
+    });
+    res.status(201).json({ message: "Successfully added!", data: newHolding });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+};
+
+const updateHolding = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, ticker, price, quantity } = req.body;
+    const updatedHolding = await prisma.stock.update({
+      where: {
+        id
+      },
+      data: {
+        name: name,
+        symbol: ticker,
+        purchasePrice: price,
+        quantity: quantity
+      }
+    });
+    res.status(200).json({ message: "Successfully updated!", data: updatedHolding });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+};
+
+const deleteHolding = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await prisma.stock.delete({
+      where: {
+        id
+      }
+    });
+    res.status(200).json({ message: "Successfully deleted!" });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+}
+
+export {
+  getAllHoldings,
+  getHoldingById,
+  addHolding,
+  updateHolding,
+  deleteHolding
+};
