dcf93a6a88251056850d61ab1e9719a258a47b28
Ayush Khatri <khatriayush2003@gmail.com>
2025-01-22 22:34:58 +0530

    fix: Create stock holding  api bug fixed

--- client/src/pages/AddStock.jsx
+++ client/src/pages/AddStock.jsx
@@ -1,23 +1,65 @@
 import { useState } from "react";
 import { IoIosArrowRoundBack } from "react-icons/io";
 import { Link } from 'react-router-dom';
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function AddStock() {
   const [symbol, setSymbol] = useState('');
   const [share, setShare] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAddingStock, setIsAddingStock] = useState(false);
+
+  const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+  const url = import.meta.env.VITE_REACT_APP_BACKEND_URL;
 
   const fetchShareDetails = async () => {
+    setIsLoading(true);
     try {
-      // const res = await fetch(`https://financialmodelingprep.com/api/v3/quote-order/${symbol.toUpperCase()}?apikey=ydSmVlnBzOvzZ3mv1hBSAP2sLBfVinz2`);
+      const res = await fetch(`https://financialmodelingprep.com/api/v3/quote-order/${symbol.toUpperCase()}?apikey=${apiKey}`);
       const data = await res.json();
       console.log(data);
       setShare(data[0]);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
-  }
+  };
+
+  const addStockToPortfolio = async () => {
+    if (!share) return alert('Please search for a stock first');
+
+    setIsAddingStock(true);
+    try {
+      const res = await fetch(`${url}/api/holding/create`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: share.name,
+          ticker: share.symbol,
+          price: share.price,
+          quantity: 1,
+          currentPrice: share.price
+        })
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        return alert(data.message);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      alert('Something went wrong!');
+    } finally {
+      setIsAddingStock(false);
+    }
+  };
+
   return (
     <section className='min-h-screen my-20 px-3'>
+
       <Link to={"/"}>
         <button className='absolute top-3 md:left-10 hover:underline flex items-center gap-2 hover:border hover:border-zinc-900 px-3 py-2 rounded-md'>
           <IoIosArrowRoundBack /> back
@@ -25,6 +67,12 @@ export default function AddStock() {
       </Link>
 
       <div className='max-w-5xl mx-auto'>
+        {isLoading &&
+          <div className='bg-teal-950/15 p-5 flex justify-between items-center rounded-md animate-pulse'>
+            <div className='w-1/4 h-8 bg-teal-900/50 rounded-md'></div>
+            <div className='w-1/4 h-8 bg-teal-900/50 rounded-md'></div>
+          </div>
+        }
         {
           share &&
           <div className='bg-teal-950/15 p-5 flex justify-between items-center rounded-md'>
@@ -47,22 +95,36 @@ export default function AddStock() {
             </div>
           </div>
         }
-        <div className='w-full flex flex-col gap-4 my-5'>
-          <div className='flex gap-3'>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            fetchShareDetails();
+          }}
+        >
+          <div className='w-full flex gap-4 my-5'>
             <input
               placeholder='Search for a stock (e.g. AAPL)'
-              type="search"
+              type="text"
+              required
               value={symbol}
               onChange={(e) => setSymbol(e.target.value)}
               className='w-full rounded-md px-3 py-2 bg-transparent border border-zinc-800'
             />
             <button
-              onClick={fetchShareDetails}
-              className='bg-teal-950 px-3 py-2 rounded-md'>Search</button>
+              type="submit"
+              disabled={isLoading || !symbol}
+              className={`${!symbol ? "bg-zinc-700 cursor-not-allowed" : "bg-teal-950"} px-3 py-2 rounded-md`}>Search</button>
           </div>
-        </div>
-        <button className='bg-teal-950 text-base font-bold px-3 py-2 rounded-md w-full'>Add to Portfolio</button>
-      </div>
+          <button
+            type="submit"
+            disabled={isAddingStock || !share}
+            onClick={addStockToPortfolio}
+            className={`${!share ? "bg-zinc-900 border border-zinc-800 cursor-not-allowed" : "bg-teal-950"}  flex justify-center items-center gap-2  text-base font-bold px-3 py-2 rounded-md w-full`}>
+            {isAddingStock && <AiOutlineLoading3Quarters className="animate-spin" />} Add to Portfolio
+          </button>
+
+        </form>
+      </div >
     </section >
   )
 }
